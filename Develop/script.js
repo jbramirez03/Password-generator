3473435ce265c72c659fad6a068ed022cd0f6d45
temptado <jason1287712@gmail.com>
2021-06-24 20:51:57 -0400

    restarted code

--- Develop/script.js
+++ Develop/script.js
@@ -1,90 +1,124 @@
 // Assignment Code
-var resultEl = document.querySelector("password");
-var generateBtn = document.querySelector("#generate");
+// var resultEl = document.querySelector("password");
+// var generateBtn = document.querySelector("#generate");
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
+// // const randomfunc = [getRandomLower, getRandomUpper, getRandomNumber, getRandomSymbol];
+// function getRandomLower () {
+//   const characters = "abcdefghijklmnopqrstuvwxyz";
+//   return characters[Math.floor(Math.random() * characters.length)];
+// };
+
+// function getRandomUpper () {
+//   const upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   return upperCharacters[Math.floor(Math.random() * upperCharacters.length)];
+// };
+
+// function getRandomNumber () {
+//   const numbers = "0123456789";
+//   return numbers[Math.floor(Math.random() * numbers.length)];
+// };
+
+// function getRandomSymbol () {
+//   const symbols = "!@#$%^&*(){}[]=<>/,.'";
+//   return symbols[Math.floor(Math.random() * symbols.length)];
+// };
+
+// const randomFunc = {
+//   lower: getRandomLower,
+//   upper: getRandomUpper,
+//   number: getRandomNumber,
+//   symbol: getRandomSymbol
+// };
 
 
-function isLength() {
+// function isLength() {
     
-  var lengthChoice = prompt("what length would you like the password to be? enter a number.", "0");
-  lengthChoice = parseInt(lengthChoice);
-  if (!lengthChoice) {
-    return;
-  }
-  return lengthChoice;
-};
+//   var lengthChoice = prompt("what length would you like the password to be? enter a number.", "0");
+//   lengthChoice = parseInt(lengthChoice);
+//   if (!lengthChoice) {
+//     return;
+//   }
+//   return lengthChoice;
+// };
 
-function isLower() {
+// function isLower() {
   
-  var lowerCasePrompt = prompt("Would you like lower case in your password? enter yes or no");
-  if (!lowerCasePrompt) {
-    return;
-  }
-  lowerCasePrompt = lowerCasePrompt.toUpperCase();
-  var lowerCaseChoice = (lowerCasePrompt === "YES");
+//   var lowerCasePrompt = prompt("Would you like lower case in your password? enter yes or no");
+//   if (!lowerCasePrompt) {
+//     return;
+//   }
+//   lowerCasePrompt = lowerCasePrompt.toUpperCase();
+//   var lowerCaseChoice = (lowerCasePrompt === "YES");
   
-  return lowerCaseChoice;
-};
+//   return lowerCaseChoice;
+// };
 
-function isUpper() {
+// function isUpper() {
   
-  var upperCasePrompt = prompt("Would you also like upper case in your password? enter yes or no.");
-  if (!upperCasePrompt) {
-    return;
-  }
-  upperCasePrompt = upperCasePrompt.toUpperCase();
-  var upperCaseChoice = (upperCasePrompt === "YES");
-  return upperCaseChoice;
-};
+//   var upperCasePrompt = prompt("Would you also like upper case in your password? enter yes or no.");
+//   if (!upperCasePrompt) {
+//     return;
+//   }
+//   upperCasePrompt = upperCasePrompt.toUpperCase();
+//   var upperCaseChoice = (upperCasePrompt === "YES");
+//   return upperCaseChoice;
+// };
 
-function isNumber() {
+// function isNumber() {
   
-  var numberChoicePrompt = prompt("would you like numbers in your password? enter yes or no.");
-  if (!numberChoicePrompt) {
-    return;
-  }
-  numberChoicePrompt = numberChoicePrompt.toUpperCase();
-  var numberChoice = (numberChoicePrompt === "YES");
-  return numberChoice;
-};
+//   var numberChoicePrompt = prompt("would you like numbers in your password? enter yes or no.");
+//   if (!numberChoicePrompt) {
+//     return;
+//   }
+//   numberChoicePrompt = numberChoicePrompt.toUpperCase();
+//   var numberChoice = (numberChoicePrompt === "YES");
+//   return numberChoice;
+// };
 
-function isSymbol() {
+// function isSymbol() {
   
-  var symbolChoicePrompt = prompt("would you like symbols in the password? enter yes or no.");
-  if (!symbolChoicePrompt) {
-    return;
-  }
-  symbolChoicePrompt = symbolChoicePrompt.toUpperCase();
-  var symbolChoice = (symbolChoicePrompt === "YES");
-return symbolChoice;
-};
+//   var symbolChoicePrompt = prompt("would you like symbols in the password? enter yes or no.");
+//   if (!symbolChoicePrompt) {
+//     return;
+//   }
+//   symbolChoicePrompt = symbolChoicePrompt.toUpperCase();
+//   var symbolChoice = (symbolChoicePrompt === "YES");
+// return symbolChoice;
+// };
 
 
-generateBtn.addEventListener("click", () => {
+// generateBtn.addEventListener("click", () => {
   
-  let generatedPassword = "";
-isLength();
+//   let generatedPassword = "";
+// isLength();
 
- var typesArr = [isLower() ,  isNumber() ,  isSymbol() ,  isUpper() ];
+//  var typesArr = [isLower(), isNumber(), isSymbol(), isUpper() ];
 
- const functionToFilter = function(isTrue) {
-   return isTrue;
- };
+//  const functionToFilter = function(isTrue) {
+//    return isTrue;
+//  };
 
- var typesArrFiltered = typesArr.filter(functionToFilter);
+//  var typesArrFiltered = typesArr.filter(functionToFilter);
 
- console.log(typesArrFiltered);
+//  console.log(typesArrFiltered);
+
+// for(let i = 0; i < parseInt(isLength()); i += typesArrFiltered.length) {
+//   typesArrFiltered.forEach(item => {
+//     const funcName = Object.values(item)[0];
+//     console.log(funcName);
+
+//     generatedPassword += randomFunc[funcName]();
+//   });
+// }
 
 
 
+
+    
   
 
+  
+  // });
 
 
 
@@ -101,7 +135,7 @@ isLength();
 
 // resultEl.innertext = generatePassword(isLower, isUpper, isNumber, isSymbol, isLength);
 
-});
+
 
 
 // var generateBtn = document.querySelector("#generate");
@@ -122,22 +156,21 @@ isLength();
 
 
 // generator functions
-function getRandomLower () {
-  const characters = "abcdefghijklmnopqrstuvwxyz";
-  return characters[Math.floor(Math.random() * characters.length)];
-}
 
-function getRandomUpper () {
-  const upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  return upperCharacters[Math.floor(Math.random() * upperCharacters.length)];
-}
+// assignment code:
 
-function getRandomNumber () {
-  const numbers = "0123456789";
-  return numbers[Math.floor(Math.random() * numbers.length)];
-}
+var length;
+var confirmNumber;
+var confirmCharacter;
+var confirmUpperCase;
+var confirmLowercase;
 
-function getRandomSymbol () {
-  const symbols = "!@#$%^&*(){}[]=<>/,.'";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
+
+// arrays containing strings that will be randomized for password making
+
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
+var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
+
+
